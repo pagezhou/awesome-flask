b64548cb70c649b8405e46a283c996c2bffb87fb
pagezz <zongpeizhou@gmail.com>
2019-03-19 12:05:35 +0800

    Update urls.py

--- mytest/mytest/urls.py
+++ mytest/mytest/urls.py
@@ -1,5 +1,7 @@
 """mytest URL Configuration
 
+test
+
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
 Examples:
